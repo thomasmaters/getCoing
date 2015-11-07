8180a82c5a0b9086ab5f3c9f39dae176c403084e
WhoopDaBase <job.stoker@gmail.com>
2015-11-07 18:54:46 +0100

    DB connectie fix

--- Website/Back-end/connect_db.php
+++ Website/Back-end/connect_db.php
@@ -5,7 +5,7 @@ $username = "witherpv_job";
 $password = "flaporen";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=witherpv_hackathon", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
